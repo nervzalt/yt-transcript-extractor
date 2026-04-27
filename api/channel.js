a7c5fe90afdb95636fb9b33f3d937ffe99b1d503
nervzalt <nervzalt@users.noreply.github.com>
2026-04-27 21:10:55 +0200

    Add dual duration slider, show duration on cards, remove select buttons

--- api/channel.js
+++ api/channel.js
@@ -68,6 +68,7 @@ export default async function handler(req, res) {
       .map(v => ({
         id: v.videoId || v.video_id || v.id,
         title: v.title || v.videoId || v.video_id || v.id,
+        duration: parseDurationSecs(v),
       }))
       .filter(v => v.id);
 
